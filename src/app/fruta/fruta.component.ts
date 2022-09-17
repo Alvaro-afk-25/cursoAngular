c3b8d70ad35731a38cb49f68a1648630646370ca
AlexKali <alex@alex.lan>
2022-09-17 17:58:05 -0500

    commit2

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -9,4 +9,12 @@ export class FrutaComponent {
    
     public nombre_componente= 'Componente de Fruta';
     public namlistado_fruta = 'naranja,manzana,pera,zandia';
+    
+    public nombre:string= 'Alvaro';
+    public edad: number=12;
+    public mayordeeada: boolean= true;
+    public trabajos:Array<any>=['carpungtero', 'albañil',12];
+    comondin:any = 10
 }
+
+
