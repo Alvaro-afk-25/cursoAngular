3c6da59b4332651f7a6d642402834adfbf74a6d4
AlexKali <alex@alex.lan>
2022-09-23 16:26:16 -0500

    commit4

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -10,17 +10,40 @@ export class FrutaComponent {
     public nombre_componente= 'Componente de Fruta';
     public namlistado_fruta = 'naranja,manzana,pera,zandia';
     
-    public nombre:string= 'Alvaro';
+    public nombre:string;
     public edad: number=12;
     public mayordeeada: boolean= true;
     public trabajos:Array<any>=['carpungtero', 'albañil',12];
-    comondin:any = 10
+    comondin:any 
+
+//PRIMERO DE EJECUTA EL CONTRUCTOR LUEGO EN ngINIT
 
     constructor(){
-        ldvlsknvlskndv
+        this.nombre='alex';
+        this.edad=25;
+        this.mayordeeada=false;
+        this.comondin= 'SI'
+        //dar un valor dentro del constructor a una variable es una buena practica
+  
 
     }
+  
 
+//para llamar funciones y realizar funciones 
+    ngOnInit(){
+        this.cambiarnombre();
+        this.cambiaredad(77);
+        alert(this.nombre+' '+this.edad);     
+    }
+
+
+    cambiarnombre(){
+        this.nombre='alvaro'
+    }
+
+    cambiaredad(edad:any){
+        this.edad=edad
+    }
 }
 
 
